c4216f276d95a51e694ac60c66c58fac9e3da69d
Diego Cordova <d.cordovaratti@gmail.com>
2025-07-12 22:12:43 -0400

    Firebase: mover las propiedades de firebaseConfig a .env

--- Backend/config/fireBaseDB.js
+++ Backend/config/fireBaseDB.js
@@ -1,24 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDJga1Td_nhErGh7WAfMP2trwEjtxHT1vM",
-  authDomain: "derivaapp.firebaseapp.com",
-  projectId: "derivaapp",
-  storageBucket: "derivaapp.firebasestorage.app",
-  messagingSenderId: "122135054847",
-  appId: "1:122135054847:web:8734fc9ee654406a58bfca"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
-// Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
