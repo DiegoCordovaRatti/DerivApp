24c8e5f2f32b60e550efa0f1a6f7e7142bfdebb3
Diego Cordova <d.cordovaratti@gmail.com>
2025-07-26 21:26:49 -0400

    frontend: creación de vistas base

---
+++ DerivApp/src/views/Perfil/Perfil.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Perfil.css';
+
+const Perfil = () => {
+  return (
+    <div className="perfil-container">
+      {}
+    </div>
+  );
+};
+
+export default Perfil; 
