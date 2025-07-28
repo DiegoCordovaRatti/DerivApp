363a9d26b3a14a2df4c6b0f601e052514f110a0e
Diego Cordova <d.cordovaratti@gmail.com>
2025-07-27 21:56:47 -0400

    frontend: instalar react router

---
+++ DerivApp/src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router/dom'
+import './Navbar.scss';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {}
+    </div>
+  );
+};
+
+export default Navbar; 
