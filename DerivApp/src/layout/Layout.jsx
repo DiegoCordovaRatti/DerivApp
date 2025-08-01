514dab4b1dc16e732856a347f8b6fe17546d61bf
Diego Cordova <d.cordovaratti@gmail.com>
2025-07-31 22:42:42 -0400

    navbar: refactorización de la barra de navegación

--- DerivApp/src/layout/Layout.jsx
+++ DerivApp/src/layout/Layout.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Outlet } from "react-router-dom";
+import { Layout as AntLayout } from 'antd';
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
+import './Layout.scss';
+
+const { Content } = AntLayout;
 
 const Layout = () => {
   return (
-    <div className='Layout'>
+    <AntLayout className='app-layout'>
       <Navbar/>
-      <Outlet/>
+      <Content className='main-content'>
+        <Outlet/>
+      </Content>
       <Footer/>
-    </div>
+    </AntLayout>
   )
 }
 
